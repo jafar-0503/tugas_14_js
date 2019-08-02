a6d2e37c3a1f70caf7852ee57ac14dbe35858f4d
Mukhamat Jafar <jafar.mukhamat7@gmail.com>
2019-08-02 22:42:12 +0700

    tugas_14_js
    tugas sudah ok !!!

---
+++ tugas-14.js
@@ -0,0 +1,13 @@
+function panggilbulat(){
+
+  var angka = 37;
+  var tgl   = 5;
+  var jumlah = angka / tgl;
+
+  console.log(jumlah);
+  console.log("");
+  console.log(Math.ceil(jumlah));
+  console.log(Math.floor(jumlah));
+  console.log(Math.round(jumlah));
+}
+panggilbulat();
